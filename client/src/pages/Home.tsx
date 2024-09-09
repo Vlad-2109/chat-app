f3d0e0e2eedd3febc568d22cc1515a17a50c6f8e
Vlad-2109 <vladswim21@gmail.com>
2024-09-09 22:01:55 +0300

    add function fetchUserDetails on client

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,28 @@
-import { Outlet } from "react-router-dom";
+import axios from 'axios';
+import { useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
 
-export const Home:React.FC = () => {
-  return <div><Outlet /></div>;
+export const Home: React.FC = () => {
+  const fetchUserDetails = async () => {
+    try {
+      const URL: string = `${import.meta.env.VITE_REACT_APP_BACKEND_URL}/api/user-details`;
+
+      const response = await axios({
+        url: URL,
+        withCredentials: true,
+      });
+      console.log('currentUserDetails', response);
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserDetails();
+  }, []);
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 };
